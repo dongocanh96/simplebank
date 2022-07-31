4e35b12f2b760025aa97c47f392a2ac01f1a107d
dongocanh96 <ngocanh6111996@gmail.com>
2022-07-31 16:05:24 +0700

    handle error for test case because of changing database schema

---
+++ db/sqlc/user_test.go
@@ -0,0 +1,59 @@
+package db
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/dongocanh96/simplebank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateUser(t *testing.T) {
+	user := createRandomUser(t)
+	testQueries.DeleteUser(context.Background(), user.Username)
+}
+
+func TestGetUser(t *testing.T) {
+	user1 := createRandomUser(t)
+	user2, err := testQueries.GetUser(context.Background(), user1.Username)
+	require.NoError(t, err)
+	require.NotEmpty(t, user2)
+
+	require.Equal(t, user1.Username, user2.Username)
+	require.Equal(t, user1.HashedPassword, user2.HashedPassword)
+	require.Equal(t, user1.FullName, user2.FullName)
+	require.Equal(t, user1.Email, user2.Email)
+	require.WithinDuration(t, user1.PasswordChangedAt, user2.PasswordChangedAt, time.Second)
+	require.WithinDuration(t, user1.CreatedAt, user2.CreatedAt, time.Second)
+
+	testQueries.DeleteUser(context.Background(), user1.Username)
+}
+
+func TestDeleteUser(t *testing.T) {
+	user := createRandomUser(t)
+	err := testQueries.DeleteUser(context.Background(), user.Username)
+	require.NoError(t, err)
+}
+
+func createRandomUser(t *testing.T) User {
+	arg := CreateUserParams{
+		Username:       util.RandomOwner(),
+		HashedPassword: "secret",
+		FullName:       util.RandomOwner(),
+		Email:          util.RandomEmail(),
+	}
+
+	user, err := testQueries.CreateUser(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, user)
+
+	require.Equal(t, arg.Username, user.Username)
+	require.Equal(t, arg.HashedPassword, user.HashedPassword)
+	require.Equal(t, arg.FullName, user.FullName)
+	require.Equal(t, arg.Email, user.Email)
+	require.NotZero(t, user.CreatedAt)
+	require.True(t, user.PasswordChangedAt.IsZero())
+
+	return user
+}
