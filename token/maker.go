b3d1eb8df2901a33118dfd3169c3601551c33e73
dongocanh96 <ngocanh6111996@gmail.com>
2022-08-01 21:02:06 +0700

    implement jwt token for auth and unit test

---
+++ token/maker.go
@@ -0,0 +1,10 @@
+package token
+
+import "time"
+
+// Maker is an interface for managing tokens
+type Maker interface {
+	CreateToken(username string, duration time.Duration) (string, error)
+
+	VerifyToken(token string) (*Payload, error)
+}
