9cda86a819d94c6786872017d2254673e021e4c1
dongocanh96 <ngocanh6111996@gmail.com>
2022-08-08 23:28:08 +0700

    add middleware and handler to method

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -84,18 +84,21 @@ func TestDeleteAccount(t *testing.T) {
 }
 
 func TestListAccounts(t *testing.T) {
+	var lastAccount Account
+
 	for i := 0; i < 10; i++ {
-		createRandomAccount(t)
+		lastAccount = createRandomAccount(t)
 	}
 
 	arg := ListAccountsParams{
+		Owner:  lastAccount.Owner,
 		Limit:  10,
 		Offset: 0,
 	}
 
 	accounts, err := testQueries.ListAccounts(context.Background(), arg)
 	require.NoError(t, err)
-	require.Len(t, accounts, 10)
+	require.NotEmpty(t, accounts)
 
 	for _, account := range accounts {
 		require.NotEmpty(t, account)
